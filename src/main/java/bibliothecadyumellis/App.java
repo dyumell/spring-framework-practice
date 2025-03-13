0e00065f44160a56b54639cb454e66947f5ef7c9
dyumell <dyumelldev@gmail.com>
2025-03-13 12:04:01 +0900

    Spring Framework 실습을 위한 Maven quickstart 템플릿

---
+++ src/main/java/bibliothecadyumellis/App.java
@@ -0,0 +1,13 @@
+package bibliothecadyumellis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
